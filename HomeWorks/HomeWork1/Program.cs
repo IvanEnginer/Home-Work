6c3ad9f87fa5764873577ac70021048f023d306a
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-04-16 13:43:21 +0300

    Init

---
+++ HomeWorks/HomeWork1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace homeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte age;
+            ulong weight;
+            float pi = 3.14f;
+            uint health;
+            int voltage;
+            double currentAmper;
+            char rank = 'A';
+            string name = "Jone";
+            ushort temperature;
+            bool openWindow;
+        }
+    }
+}
