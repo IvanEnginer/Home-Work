7b3137aa1a270540071a02683e3ca8e0d4a12d04
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-04-23 15:13:13 +0300

    Update

--- HomeWorks/work_2.3/Program.cs
+++ HomeWorks/work_2.3/Program.cs
@@ -6,23 +6,14 @@ namespace work_2._3
     {
         static void Main(string[] args)
         {
-            //            for(int i = 5; i <= 96; i += 7)Console.WriteLine($"{i}");
             int chekPoint;
             int startPoint = 5;
             int endPoint = 96;
             int iterator = 7;
 
-            //chekPoint = startPoint;
-            //Console.WriteLine(chekPoint);
-            //while (chekPoint != endPoint)
-            //{
-            //    chekPoint = chekPoint + iterator;
-            //    Console.WriteLine(chekPoint);                    
-            //}
+            for (chekPoint = startPoint; chekPoint <= endPoint; chekPoint += iterator) 
+                Console.WriteLine(chekPoint);
 
-            for (chekPoint = startPoint; chekPoint <= endPoint; chekPoint += iterator) Console.WriteLine(chekPoint);
-
-            // в for итерация происходит в условии цикла, что на мой взгял удобнее 
         }
     }
 }
