3842409a9a63516142a0a1644ab78880eed6ad78
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-04-26 20:35:41 +0300

    Update 2

--- HomeWorks/Work7/Program.cs
+++ HomeWorks/Work7/Program.cs
@@ -6,21 +6,6 @@ namespace Work7
     {
         static void Main(string[] args)
         {
-            //// minutsInQuea = peoplsInQeua /timeScope
-            //int peoplsInQeua;
-            //int timeScope = 10;
-            //int hourInQuea;
-            //int minutsInQuea;
-            //// int pepolsToUseInHours = 60/timeScope;
-
-            //Console.Write("Количество человек в очереди : ");
-            //peoplsInQeua = Convert.ToInt32(Console.ReadLine());
-
-            //hourInQuea = peoplsInQeua / timeScope;
-
-            //minutsInQuea = (peoplsInQeua / timeScope)*10;
-            //Console.WriteLine($"Вы должны отстоять в очереди {hourInQuea} часа и {minutsInQuea} минут.");
-
             int peoplsInQeua;
             int timeScope = 10;
             int hourInQuea;
