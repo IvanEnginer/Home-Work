61837d58093c1f09015e295efcba663537f5ff2f
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-04-26 20:34:26 +0300

    Update 1

--- HomeWorks/Work7/Program.cs
+++ HomeWorks/Work7/Program.cs
@@ -6,19 +6,34 @@ namespace Work7
     {
         static void Main(string[] args)
         {
-            // minutsInQuea = peoplsInQeua /timeScope
+            //// minutsInQuea = peoplsInQeua /timeScope
+            //int peoplsInQeua;
+            //int timeScope = 10;
+            //int hourInQuea;
+            //int minutsInQuea;
+            //// int pepolsToUseInHours = 60/timeScope;
+
+            //Console.Write("Количество человек в очереди : ");
+            //peoplsInQeua = Convert.ToInt32(Console.ReadLine());
+
+            //hourInQuea = peoplsInQeua / timeScope;
+
+            //minutsInQuea = (peoplsInQeua / timeScope)*10;
+            //Console.WriteLine($"Вы должны отстоять в очереди {hourInQuea} часа и {minutsInQuea} минут.");
+
             int peoplsInQeua;
             int timeScope = 10;
             int hourInQuea;
+            int totalMinutsInQuea;
             int minutsInQuea;
-            // int pepolsToUseInHours = 60/timeScope;
 
             Console.Write("Количество человек в очереди : ");
             peoplsInQeua = Convert.ToInt32(Console.ReadLine());
 
-            hourInQuea = peoplsInQeua / timeScope;
-            
-            minutsInQuea = (peoplsInQeua / timeScope)*10;
+            totalMinutsInQuea = peoplsInQeua * timeScope;
+            hourInQuea = totalMinutsInQuea / 60;
+            minutsInQuea = totalMinutsInQuea - (hourInQuea * 60);
+
             Console.WriteLine($"Вы должны отстоять в очереди {hourInQuea} часа и {minutsInQuea} минут.");
         }
     }
--- HomeWorks/work_2.4/Program.cs
+++ HomeWorks/work_2.4/Program.cs
@@ -6,6 +6,10 @@ namespace work_2._4
     {
         static void Main(string[] args)
         {
+            //С помощью Random получить число number, которое не больше 100.
+            //Найти сумму всех положительных чисел меньше number(включая число), 
+            //которые кратные 3 или 5. (К примеру, это числа 3, 5, 6, 9, 10, 12, 15 и т.д.)
+
             Random random = new Random();
 
             int number = random.Next(0, 101);
@@ -13,6 +17,45 @@ namespace work_2._4
             int totalsum = 0;
             int multipleNumer1 = 3;
             int multipleNumer2 = 5;
+
+            int totalIterator;
+            totalIterator = number / multipleNumer1 + number / multipleNumer2;
+
+            //           if(number % multipleNumer1 + number % multipleNumer2 == 0)
+
+            // if (number % multipleNumer1 == 0 ? number % multipleNumer2 == 0)
+
+
+
+            Console.WriteLine(totalIterator);
+
+            for(int i = 0; i <= number; i++)
+            {
+                if (i % multipleNumer1 == 0)
+                {
+                    summ += multipleNumer1;
+                }
+
+                else if (i % multipleNumer2 == 0)
+                    summ += multipleNumer2;
+            }
+
+            Console.WriteLine("number : " + number + ", total summ : " + summ);
+
+            //             string GetWeatherDisplay(double tempInCelsius) => tempInCelsius < 20.0 ? "Cold." : "Perfect!";
+
+            //for (int i = 0; i < totalIterator; i++)
+            //{ 
+
+            //}
+
+            //Random random = new Random();
+
+            //int number = random.Next(0, 101);
+            //int summ = 0;
+            //int totalsum = 0;
+            //int multipleNumer1 = 3;
+            //int multipleNumer2 = 5;
 
             for (int i = 0; i < number / multipleNumer1; i++)
             {
@@ -22,7 +65,7 @@ namespace work_2._4
 
             int iterator = 0;
 
-            while(iterator < number / multipleNumer2)
+            while (iterator < number / multipleNumer2)
             {
                 summ += multipleNumer2;
                 totalsum += summ;
