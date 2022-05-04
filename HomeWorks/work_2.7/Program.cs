afedaf1a1ca472565644738f0fb015b5d8a9c5b4
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-05-04 21:51:23 +0300

    Up

--- HomeWorks/work_2.7/Program.cs
+++ HomeWorks/work_2.7/Program.cs
@@ -6,14 +6,12 @@ namespace work_2._7
     {
         static void Main(string[] args)
         {
-            //1.Стоит собрать строку из символов через цикл string += char; и вывести строку 2 раза.
 
             string userString;
             string userSign;
+            string outString = "";
 
             int lenghUserString;
-            int lines = 3;
-            int numberUserStringLine = 2;
             int numberStartsSimbol = 1;
             int numberEndSimbol = 1;
 
@@ -27,25 +25,11 @@ namespace work_2._7
 
             for(int i = 0; i < lenghUserString + numberStartsSimbol + numberEndSimbol; i++)
             {
-
+                outString += userSign;
             }
 
-            //for(int i = 1; i < lines; i++)
-            //{
-
-            //    //if(i == numberUserStringLine)
-            //    //{
-            //    //    Console.WriteLine(userSign + userString + userSign);
-            //    //}
-            //    //else
-            //    //{
-            //    //    for(int j = 0; j < lenghUserString + numberStartsSimbol + numberEndSimbol; j++)
-            //    //    {
-            //    //        Console.Write(userSign);
-            //    //    }
-            //    //    Console.WriteLine();
-            //    //}
-            //}
+            Console.WriteLine(outString);
+            Console.WriteLine(userSign + userString + userSign + "\n" + outString);
         }
     }
 }
