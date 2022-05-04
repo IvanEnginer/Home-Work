5b4e963b9dae62a3b9ecc6f9214de8aadd2d8a27
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-05-04 22:34:35 +0300

    update

---
+++ HomeWorks/work 2.8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace work_2._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sicretMasseg = "Hello World!";
+            string password = "qwerty";
+
+            int tryChekPassword = 3;
+
+            for (int i = 0; i < tryChekPassword; i++)
+            {
+                Console.Write("Enter Password: ");
+
+                if (Console.ReadLine() == password)
+                {
+                    Console.WriteLine(sicretMasseg);
+                    i  = tryChekPassword;
+                }
+            }
+        }
+    }
+}
