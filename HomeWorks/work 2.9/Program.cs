75ed43f6ac2368d773460f8ddfd4280509c03199
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-05-06 19:43:50 +0300

    update

---
+++ HomeWorks/work 2.9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace work_2._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int N = random.Next(1, 27);
+            int startPoint = N;
+            int startReng = 100;
+            int endReng = 1000;
+            int amoundMultipelNumbers = 0;
+
+            Console.WriteLine("N " + N);
+
+            while(startPoint < startReng)
+            {
+                startPoint += N;
+            }
+
+            Console.WriteLine("Start POint " + startPoint);
+
+            for (int i = startPoint; i < endReng; i += N)
+            {
+                amoundMultipelNumbers++;
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("amoundMultipelNumbers " + amoundMultipelNumbers);
+        }
+    }
+}
