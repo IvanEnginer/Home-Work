96c2c4f263e0ac4749b1ef041a4c45a8452aa8f3
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-05-06 20:07:38 +0300

    Update

--- HomeWorks/2.10/Program.cs
+++ HomeWorks/2.10/Program.cs
@@ -6,14 +6,22 @@ namespace _2._10
     {
         static void Main(string[] args)
         {
-            //            Найдите минимальную степень двойки, превосходящую заданное число.
-            //К примеру, для числа 4 будет 2 в степени 3, то есть 8. 4 < 8.
-            //Для числа 29 будет 2 в степени 5, то есть 32. 29 < 32.
-            //В консоль вывести число(лучше получить от Random), степень и само число 2 в найденной степени.
-
             Random random = new Random();
-            int healthBoss = random.Next(250, 500);
-            Console.WriteLine("Hello World!");
+            int startRandomBorder = 1;
+            int endRandomBorder = 100;
+            int Number = random.Next(startRandomBorder, endRandomBorder);
+            int fundation = 2;
+            int result = fundation;
+            int degree = 1;
+
+            while(result <= Number)
+            {
+                result += result;
+                degree++;
+            }
+
+            Console.WriteLine($"result  {result} \n" +
+                $"degree {degree}");
         }
     }
 }
