f3c0d574e42f221f72ad30b90916950a2cd68df6
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-05-24 20:43:14 +0300

    update

---
+++ HomeWorks/work 4.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int posiition;
+            int percent;
+
+            Console.WriteLine("Enter position");
+            posiition = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter porcent");
+            percent = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+            creatBar(posiition, percent);
+        }
+
+        static void creatBar(int position, int percent)
+        {
+            int columInBar = 10;
+            int newPersent = percent / columInBar;
+
+
+            string bar = "[";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            for (int i = 0; i < newPersent; i++)
+            {
+                bar += "#";
+            }
+
+            for (int i = newPersent + 1; i <= columInBar; i++)
+            {
+                bar += "_";
+            }
+
+            bar += "]";
+
+            Console.SetCursorPosition(0, position);
+            Console.WriteLine(bar);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
