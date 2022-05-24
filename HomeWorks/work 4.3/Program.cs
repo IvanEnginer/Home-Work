609b36fcf66c66d752f33591d051f599f41c5c1b
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-05-24 20:45:22 +0300

    upd

---
+++ HomeWorks/work 4.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            chekuserMaseg();
+        }
+
+        static int chekuserMaseg()
+        {
+            bool tryIsWhile = true;
+            int number = 0;
+
+            while (tryIsWhile)
+            {
+                string massege = Console.ReadLine();
+
+                if (int.TryParse(massege, out number))
+                {
+                    Console.WriteLine("You Number " + number);
+                    tryIsWhile = false;
+                }
+                else
+                {
+                    Console.WriteLine("It is not number, try again");
+                }
+
+            }
+
+            return number;
+        }
+    }
+}
