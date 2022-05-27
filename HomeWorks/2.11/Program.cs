4937e9753f906647e449ffa9a33e014cf63f0f20
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-05-27 15:57:09 +0300

    upddate27.1

---
+++ HomeWorks/2.11/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _2._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringForAnalis = ")(()((())))";
+
+            int deap = 0;
+            int maximumDeap = 0;
+
+//            bool vereficationIsAlowwed = true;
+
+            bool vereficationIsSuccefule;
+
+
+            for (int i = 0; i < stringForAnalis.Length; i++)
+            {
+                if (stringForAnalis[i] == '(')
+                {
+
+                    deap++;
+
+                    if (deap > maximumDeap)
+                        maximumDeap = deap;
+
+                }
+                else
+                    deap--;
+
+                if (deap < 0)
+                {
+
+                    vereficationIsSuccefule = false;
+                    break;
+
+                }
+                else 
+                {
+                    vereficationIsSuccefule = true;
+                }                
+            }
+
+            if (deap == 0)
+                Console.WriteLine(maximumDeap);
+            else
+                Console.WriteLine("fail");
+        }
+    }
+}
