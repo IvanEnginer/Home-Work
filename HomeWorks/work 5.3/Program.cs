4079f1b18bf5dfdf37ccf0180b6ae93d075b29b0
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-05-31 19:43:42 +0300

    update

--- HomeWorks/work 5.3/Program.cs
+++ HomeWorks/work 5.3/Program.cs
@@ -11,30 +11,33 @@ namespace work_5._3
 
             bool continueCounting = true;
 
-            int sum = 0;
-
             while (continueCounting)
             {
                 Console.WriteLine("Введите число, exit или sum: ");
                 string messageUser = Console.ReadLine();
 
                 if (int.TryParse(messageUser, out int number))
-                {
                     numbers.Add(number);
-                }
 
-                if(messageUser == "sum")
-                {
-                    for (int i = 0; i < numbers.Count; i++)
-                        sum += numbers[i];
-
-                    Console.WriteLine("Sum = " + sum);
-                }
+ //               if (messageUser == "sum")
+                    Sum(messageUser, numbers);
                 
                 if(messageUser == "exit")
-                {
                     continueCounting = false;
-                }
+            }
+        }
+
+        static void Sum (string messageUser, List<int> numbers)
+        {
+
+            if (messageUser == "sum")
+            {
+                int sum = 0;
+
+                for (int i = 0; i < numbers.Count; i++)
+                    sum += numbers[i];
+
+                Console.WriteLine("Sum = " + sum);
             }
         }
     }
