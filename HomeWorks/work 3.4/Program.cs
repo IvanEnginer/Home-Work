46d901f918d3565b055c49c6712a96a506c83595
IvanEnginer <103309995+IvanEnginer@users.noreply.github.com>
2022-06-05 14:04:59 +0300

    update

--- HomeWorks/work 3.4/Program.cs
+++ HomeWorks/work 3.4/Program.cs
@@ -6,60 +6,37 @@ namespace work_3._4
     {
         static void Main(string[] args)
         {
-            //int lengthArray = 0;
-            //int[] arrayNumbers;
-            //int[] arrayCopyNumbers = ;
+            int[] arrayNumbers = new int [0];
+            bool enableCounter = true;
+            string messageUser;
 
-            //bool enableCounter = true;
+            while(enableCounter)
+            {
+                Console.WriteLine("Введите число, exit или sum: ");
+                messageUser = Console.ReadLine();
 
-            //string messageUser;
+                if (messageUser != "sum" && messageUser != "exit")
+                {
+                    int[] tempArray = new int[arrayNumbers.Length + 1];
 
-            //while (enableCounter)
-            //{
-            //    Console.WriteLine("Введите число, exit или sum: ");
-            //    messageUser = Console.ReadLine();
+                    for (int i = 0; i < arrayNumbers.Length; i++)
+                        tempArray[i] = arrayNumbers[i];
 
-            //    if (messageUser != "sum" && messageUser != "exit")
-            //    {
-            //        int number = Convert.ToInt32(messageUser);
+                    tempArray[tempArray.Length - 1] = Convert.ToInt32(messageUser);
+                    arrayNumbers = tempArray;    
+                } else if(messageUser == "sum")
+                {
+                    int sum = 0;
 
-            //        arrayCopyNumbers = new int[];
+                    for (int i = 0; i < arrayNumbers.Length; i++)
+                        sum += arrayNumbers[i];
 
-            //        lengthArray += 1;
-            //        arrayNumbers = new int[lengthArray];
-
-            //        for (int i = 0; i < arrayCopyNumbers.Length; i++)
-            //        {
-            //            arrayNumbers[i] = arrayCopyNumbers[i];
-            //        }
-
-            //        arrayNumbers[lengthArray - 1] = number;
-            //        arrayCopyNumbers = new int[lengthArray];
-
-            //        for (int i = 0; i < arrayNumbers.Length; i++)
-            //        {
-            //            arrayCopyNumbers[i] = arrayNumbers[i];
-            //        }
-
-            //        arrayCopyNumbers[lengthArray - 1] = number;
-            //    }
-
-            //    else if (messageUser == "sum")
-            //    {
-            //        int arraySum = 0;
-
-            //        for (int i = 0; i < arrayNumbers.Length; i++)
-            //        arraySum += arrayNumbers[i];
-
-            //        Console.WriteLine("Сумма массива: " + arraySum);
-            //        arraySum = 0;
-            //    }
-
-            //    else if (messageUser == "exit")
-            //    {
-            //        enableCounter = false;
-            //    }
-            //}
+                    Console.WriteLine("Sum = " + sum);
+                } else if(messageUser == "exit")
+                {
+                    enableCounter = false;
+                }              
+            }
         }
     }
 }
---
+++ HomeWorks/work 5.5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace work_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[] {1, 2, 1};
+            int[] array2 = new int[] { 3, 2};
+            //"1", "2", "1"} + {"3", "2"} => {"1", "2", "3"} 
+
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            List<int> list3 = new List<int>();
+
+            for (int i = 0; i < array1.Length; i++)
+                list1.Add(array1[i]);
+
+            for (int i = 0; i < array2.Length; i++)
+                list1.Add(array2[i]);
+
+            for (int i = 0; i < list1.Count; i++)
+                Console.Write("\t" + list1[i]);
+
+            Console.WriteLine();
+
+            for(int i = 0; i < list1.Count; i++)
+            {
+               
+            }
+
+
+                
+
+            //for (int i = 0; i < list2.Count; i++)
+            //    Console.Write("\t" + list2[i]);
+
+            //Console.WriteLine(" Add ");
+
+            //for (int i = 0; i < list2.Count; i++)
+            //    list1.Add(list2[i]);
+
+            //for (int i = 0; i < list1.Count; i++)
+            //    Console.Write("\t" + list1[i]);
+
+            Console.WriteLine();
+
+            //if(list1.Contains(list2[1]))
+
+            //for (int i = 0; i < list1.Count; i++)
+            //{ 
+            //    foreach(int number in list2)
+            //    {
+
+            //    }
+            //}
+        }
+    }
+}
